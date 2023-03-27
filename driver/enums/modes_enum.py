a7819739ed957a026ea582ca69aab3af9f7fdf7c
psp515 <kolberu@gmail.com>
2023-03-27 14:24:58 +0200

    Update modes_enum.py

--- driver/enums/modes_enum.py
+++ driver/enums/modes_enum.py
@@ -1,6 +1,13 @@
 
 
 class Modes:
-    ConstantNA = -1,
-    Constant = 0,
-    RGB = 1
+    CONSTANT = 0,
+    CONSTANT_NA = 1,
+    RGB = 2,
+    LOADING = 3,
+    WIFI = 4,
+    BLINK = 5,
+    INSTANT_BLINK = 6,
+    SPINNING = 7,
+    TORCH = 8,
+    FADE = 9
